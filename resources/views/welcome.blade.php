266f4c9b97a3a84d726c646bd89f0d98d3d11d4c
shohan494 <shohan12224943@gmail.com>
2018-06-04 20:48:29 +0600

    laravel vue js with model Company

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,33 +83,7 @@
                 </div>
 
                 <div>          
-                    @php
-
-{{
-                    function get_client_ip() {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
-
-
-                    dd(geoip($ip = "168.9.9.9"));
-
-                    }}
-                    @endphp
+WELCOME
                 </div>
 
                 <div class="links">
