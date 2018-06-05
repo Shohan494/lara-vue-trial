bd0db5e9554aa06309a96aeedc6e4f5697a5b4ec
shohan494 <shohan12224943@gmail.com>
2018-06-06 01:17:02 +0600

    uses of single component in laravel

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <p>{{ greeting }} World!</p>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+module.exports = {
+  data: function () {
+    return {
+      greeting: 'Hello'
     }
+  }
+}
 </script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
